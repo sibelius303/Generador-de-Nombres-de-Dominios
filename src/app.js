401ce8bfd38acf680823c97ec75cf6649f28ef9c
sibelius303 <cesarev17@gmail.com>
2022-08-24 23:54:23 +0000

    Segunda revision con for

--- src/app.js
+++ src/app.js
@@ -5,32 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-var pronoun = ["the", "our", "we", "el"];
-var adj = ["great", "big", "power", "gran"];
-var noun = ["jogger", "racoon", "lion", "boleta"];
-var com = [".com", ".net", ".org", ".blogg"];
-var pais = [".es", ".bra", ".ve", ".co"];
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var com = [".com", ".net"];
 
-var pronounRandom = Math.floor(Math.random() * pronoun.length);
-var pronounDato = pronoun[pronounRandom];
-
-var adjRandom = Math.floor(Math.random() * adj.length);
-var adjDato = adj[adjRandom];
-
-var nounRandom = Math.floor(Math.random() * noun.length);
-var nounDato = noun[nounRandom];
-
-var comRandom = Math.floor(Math.random() * com.length);
-var comDato = com[comRandom];
-
-var paisRandom = Math.floor(Math.random() * pais.length);
-var paisDato = pais[paisRandom];
-
-var resul = "www." + pronounDato + adjDato + nounDato + comDato + paisDato;
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let l = 0; l < noun.length; l++) {
+      for (let t = 0; t < com.length; t++) {
+        var resul = pronoun[i] + adj[j] + noun[l] + com[t];
+        console.log(pronoun[i] + adj[j] + noun[l] + com[t]);
+      }
+    }
+  }
+}
 
 var dominio = document.getElementById("dominio");
 dominio.innerHTML = resul;
